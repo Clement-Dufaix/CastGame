67858029de0404f3a0195a3fedb12ccc2d4aa555
Thomas <tvuillemin@orange.fr>
2017-11-08 16:42:48 +0100

    Passage vers Fragments

--- app/src/main/java/pts3/castgame/activities/MainActivity.java
+++ app/src/main/java/pts3/castgame/activities/MainActivity.java
@@ -2,25 +2,43 @@ package pts3.castgame.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import pts3.castgame.R;
+import pts3.castgame.fragments.TemplateFragment;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getIntent();
     }
+
 
     public void choisirDifficulte(View view) {
-        Intent intent = new Intent(this, ChoixJeuActivity.class);
-        startActivity(intent);
+
+        TemplateFragment carteFragment = (TemplateFragment) getSupportFragmentManager().findFragmentById(R.id.frame_choix_template);
+
+        TemplateFragment newFragment = new TemplateFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        // On remplace l'ancien fragment par le nouveau.
+        transaction.replace(R.id.fragment, newFragment);
+        // transaction.addToBackStack();
+
+        // On commit la transaction.
+        transaction.commit();
     }
 
+    /**
+     * On laisse pour le moment le fait de changer d'activité car la carte ne reprend rien du reste.
+     * (A voir)
+     *
+     * @param view
+     */
     public void voirMap(View view) {
         Intent intent = new Intent(this, MapActivity.class);
         startActivity(intent);
