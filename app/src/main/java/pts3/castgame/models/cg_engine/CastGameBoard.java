200dd00c716ee71efc196c14be62852d0ba0c21f
Thomas <tvuillemin@orange.fr>
2018-01-08 17:15:21 +0100

    Suppression des classes inutiles

--- app/src/main/java/pts3/castgame/models/cg_engine/CastGameBoard.java
+++
@@ -1,6 +0,0 @@
-package pts3.castgame.models.cg_engine;
-
-// Jamais utilisée, que veux-tu faire avec un plateau ?
-public class CastGameBoard {
-
-}
--- app/src/main/java/pts3/castgame/models/cg_engine/CastGameCode.java
+++
@@ -1,7 +0,0 @@
-package pts3.castgame.models.cg_engine;
-
-// Cette classe est jamais utilisée nn ?
-public class CastGameCode {
-
-    private CastGameTemplate template;
-}
