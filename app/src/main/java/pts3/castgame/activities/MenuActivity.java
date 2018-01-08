5da9eba97d5df3a2327a93f2492512b9ca3bc9fd
Thomas <tvuillemin@orange.fr>
2018-01-08 20:53:58 +0100

    Animation simple sur le menu

--- app/src/main/java/pts3/castgame/activities/MenuActivity.java
+++ app/src/main/java/pts3/castgame/activities/MenuActivity.java
@@ -2,16 +2,27 @@ package pts3.castgame.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceScreen;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.widget.ImageButton;
 
 import pts3.castgame.R;
 
 public class MenuActivity extends AppCompatActivity {
 
+    private ImageButton img;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        img = (ImageButton) findViewById(R.id.anim_image);
+        img.animate().translationY(-600);
+
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -20,11 +31,11 @@ public class MenuActivity extends AppCompatActivity {
                     Intent i = new Intent(getApplicationContext(), MainActivity.class);
                     startActivity(i);
                 } catch (InterruptedException e) {
-                    // TODO Auto-generated catch block
                     e.printStackTrace();
                 }
             }
         }).start();
 
     }
+
 }
