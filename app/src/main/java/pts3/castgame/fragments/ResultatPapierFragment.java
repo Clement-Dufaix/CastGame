738e735185377974496c860389fcc6a73c7444ed
Thomas <tvuillemin@orange.fr>
2018-01-10 18:28:55 +0100

    IHM CompanionResult Liens des éléments graphiques dans le fragment

--- app/src/main/java/pts3/castgame/fragments/ResultatPapierFragment.java
+++ app/src/main/java/pts3/castgame/fragments/ResultatPapierFragment.java
@@ -5,31 +5,37 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import pts3.castgame.R;
 import pts3.castgame.activities.MainActivity;
-
+import pts3.castgame.models.lien.FacadeMoteur;
 
 public class ResultatPapierFragment extends Fragment {
 
     TextView template;
-    TextView compilePasTextView;
-    TextView executePasTextView;
-    TextView afficheTextView;
+    ImageView compilationImageView;
+    ImageView executionImageView;
+    ImageView displayImageView;
+    TextView displayTextView;
+    FacadeMoteur facade;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_answer_companion, container, false);
+
+        MainActivity mainActivity = (MainActivity) getActivity();
+        facade = mainActivity.getFacade();
         template = v.findViewById(R.id.templateReponseTextView);
+        compilationImageView = v.findViewById(R.id.icon_compilation);
+        executionImageView = v.findViewById(R.id.icon_execution);
+        displayImageView = v.findViewById(R.id.icon_display);
+        displayTextView = v.findViewById(R.id.text_display);
         template.setText(((MainActivity) getActivity()).getIntent().getStringExtra("template"));
-        // compilePasTextView = v.findViewById(R.id.compilePasTextView);
-        // compilePasTextView.setVisibility(View.VISIBLE);
-        // executePasTextView = v.findViewById(R.id.executePasTextView);
-        // compilePasTextView.setVisibility(View.VISIBLE);
-        // executePasTextView = v.findViewById(R.id.executePasTextView);
-        // executePasTextView.setVisibility(View.VISIBLE);
-        // executePasTextView.setText("Affiche :\n'La carte'");
+
+        // Pas pour les templates utilisant des méthodes...
+        displayTextView.setText("Affichage\nen sortie");
 
         return v;
     }
