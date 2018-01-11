32e2686daea13ff6afb78f3383a354107d530efd
Thomas <tvuillemin@orange.fr>
2018-01-11 21:26:59 +0100

    Retour menu fonctionnel Quitter appli fonctionnel Retrait du quitter partie en cours Retrait de contentMain.xml (inutile) Retrait des éléments xml résidus du tuto drawer

--- app/src/main/java/pts3/castgame/activities/MainActivity.java
+++ app/src/main/java/pts3/castgame/activities/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityManagerCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -41,22 +42,22 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
         intentMap = new Intent(this, MapActivity.class);
         facadeMoteur = new FacadeMoteur();
 
-        bMap = (ImageButton) findViewById(R.id.b_map);
+        bMap = findViewById(R.id.b_map);
         bMap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -68,7 +69,7 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -164,20 +165,22 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_accueil) {
-            // Handle the camera action
-        } else if (id == R.id.nav_aide) {
-
-        } else if (id == R.id.nav_giveup) {
+        if (id == R.id.nav_home) {
+            // Faudra demander à l'utilisateur s'il est sûr avant de revenir à l'accueil.
+            Intent intent = new Intent(this, MainActivity.class);
+            // Nettoyage de la pile
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+        } else if (id == R.id.nav_help) {
 
         } else if (id == R.id.nav_exit) {
-
+            this.finish();
+            System.exit(0);
         } else if (id == R.id.nav_about) {
 
         }
 
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
